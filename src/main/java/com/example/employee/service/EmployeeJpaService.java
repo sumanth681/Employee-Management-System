a3be8f642ef9f3b0562db9e32ff9241988a78175
Sumanth Neela <neelasumanth681@gmail.com>
2023-12-25 19:56:30 +0000

    solution

---
+++ src/main/java/com/example/employee/service/EmployeeJpaService.java
@@ -0,0 +1,73 @@
+package com.example.employee.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.*;
+
+import com.example.employee.model.Employee;
+import com.example.employee.repository.EmployeeRepository;
+import com.example.employee.repository.EmployeeJpaRepository;
+
+@Service
+public class EmployeeJpaService implements EmployeeRepository {
+    @Autowired
+    public EmployeeJpaRepository employeeJpaRepository;
+
+    @Override
+    public ArrayList<Employee> getEmployees() {
+
+        List<Employee> employeeList = employeeJpaRepository.findAll();
+        ArrayList<Employee> employees = new ArrayList<>(employeeList);
+        return employees;
+    }
+
+    @Override
+    public Employee getEmployeeById(int employeeId) {
+        try {
+            Employee employee = employeeJpaRepository.findById(employeeId).get();
+            return employee;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public Employee addEmployee(Employee employee) {
+        employeeJpaRepository.save(employee);
+        return employee;
+    }
+
+    @Override
+    public Employee updateEmployee(int employeeId, Employee employee) {
+        try {
+            Employee newEmployee = employeeJpaRepository.findById(employeeId).get();
+            if (employee.getEmployeeName() != null) {
+                newEmployee.setEmployeeName(employee.getEmployeeName());
+            }
+            if (employee.getEmail() != null) {
+                newEmployee.setEmail(employee.getEmail());
+            }
+            if (employee.getDepartment() != null) {
+                newEmployee.setDepartment(employee.getDepartment());
+            }
+            employeeJpaRepository.save(newEmployee);
+            return newEmployee;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @Override
+    public void deleteEmployee(int employeeId) {
+        try {
+            employeeJpaRepository.deleteById(employeeId);
+
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
